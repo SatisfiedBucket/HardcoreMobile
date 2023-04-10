052bf8467856cab5385fd10cca36a2f11b167f31
SatisfiedBucket <117303947+SatisfiedBucket@users.noreply.github.com>
2023-04-10 09:36:43 +0100

    Update Sprint.lua

--- Sprint.lua
+++ Sprint.lua
@@ -1,5 +1,12 @@
 -- Made by V5#7463, are you happy now?
 
+if not game.Players.LocalPlayer.PlayerGui:FindFirstChild("StaminaGui") then
+    require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).titlelocation("Execute hardcore first idiot")
+    require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Execute hardcore first idiot")
+
+    while wait() do end
+end
+
 local Sprint = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
 local UIStroke = Instance.new("UIStroke")
@@ -23,7 +30,7 @@ UICorner.CornerRadius = UDim.new(0.5, 0)
 UICorner.Parent = Sprint
 
 UIStroke.Thickness = 3
-UIStroke.Color = Color3.new(255, 222, 189)
+UIStroke.Color = Color3.new(1, 0.8705882353, 0.7411764706)
 UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke.LineJoinMode = Enum.LineJoinMode.Round
 UIStroke.Parent = Sprint
@@ -39,11 +46,17 @@ Icon.Image = "rbxassetid://2195446979"
 Icon.ImageColor3 = Color3.fromRGB(255, 222, 189)
 Icon.ScaleType = Enum.ScaleType.Fit
 
+if not getgenv().Settings then
+    getgenv().Settings = {
+        DisableBeforeOut = true
+    }
+end
+
 local Sprinting = false
 
 game:GetService("VirtualInputManager"):SendKeyEvent(false, "Q" ,false ,game)
 
-Sprint.MouseButton1Click:Connect(function()
+Sprint.MouseEnter:Connect(function()
     if Sprinting then
         game:GetService("VirtualInputManager"):SendKeyEvent(false, "Q" ,false ,game)
         Icon.Image = "rbxassetid://2195446979"
@@ -54,5 +67,17 @@ Sprint.MouseButton1Click:Connect(function()
         Icon.Image = "rbxassetid://1202200114"
 
         Sprinting = true
+    end
+end)
+
+spawn(function()
+    while getgenv().Settings.DisableBeforeOut do
+        wait()
+        if game.Players.LocalPlayer.PlayerGui.StaminaGui.Sprint.Bar.Fill.Size.X.Scale < 0.021 and Sprinting then
+            game:GetService("VirtualInputManager"):SendKeyEvent(false, "Q" ,false ,game)
+            Icon.Image = "rbxassetid://2195446979"
+    
+            Sprinting = false
+        end
     end
 end)
